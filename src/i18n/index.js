c35bfe08867c316a737e136f8e601f35df36facd
BorysPresn <boryspresniak@gmail.com>
2026-02-17 12:40:29 +0100

    update localization fallback to use DEFAULT_LANG constant

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,11 +2,12 @@ const pl = require("./dictonaries/pl");
 const en = require("./dictonaries/en");
 const ua = require("./dictonaries/ua");
 const ru = require("./dictonaries/ru");
+const { DEFAULT_LANG } = require("../config/constants");
 
 const dict = { pl, en, ua, ru };
 
 function getLocalizedText(lang, key) {
-  return dict[lang]?.[key] || dict.pl[key] || key;
+  return dict[lang]?.[key] || dict[DEFAULT_LANG][key] || key;
 }
 
 module.exports = { getLocalizedText };
